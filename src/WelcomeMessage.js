74f6a5d387477bec50167f29acbed9a124aa4369
kjeffreys <kjeffreyscs@gmail.com>
2023-12-13 21:32:10 -0500

    CHRISTMAS QUARTERFINALSgit add .!

--- src/WelcomeMessage.js
+++ src/WelcomeMessage.js
@@ -8,78 +8,63 @@ function WelcomeMessage()
     return (
         <div className="container">
             <div className="header">
-                <h1>Week 10 Awards!</h1>
-                <h2><strong>👑Top Team Score</strong>: 150.36 pts -- Lucy's Head Coach
-                </h2>
-                <h2><strong>👑Top Player Score</strong>: Dak Prescott -- 39.86 pts -- Dean's Definitive Team
-                </h2>
-                <h2><strong>👑Biggest Win</strong>: 64.76 pts -- Lucy's Head Coach
-                </h2>
-                <h2><strong>Fail Snail</strong>: 🐌 No Barkley All Bite 🐌 -- Bill fell 12 pts short of projection. So it seems everyone did well vs. projections this week honestly... </h2>
-                <img src={snailGif} alt="Snail GIF" style={{ width: '100px', height: '100px' }} />
-
+                <h1>Week 14 Awards!</h1>
             </div>
 
             <section className="announcement">
-                <h2>Week 10 recap</h2>
-                <p>
-                    We're close now. Very close
-                </p>
-            </section>
+                <h2><strong>👑Top Team Score</strong>: 181.52 pts -- Espresso LoPresto
+                </h2>
+                <h2><strong>👑Top Player Score</strong>: Lamar Jackson -- 33.64 pts -- The Unit
+                </h2>
+                <h2><strong>👑Biggest Win</strong>: 75 pts -- Espresso LoPres(t)o
+                </h2>
+                <h2><strong>Fail Snail</strong>: 🐌 Hanah's Nifty Team 🐌 -- Hanah fell 29 pts short of projection. But at least it only came off the back of losing her quarterback right before playoffs...yeesh! </h2>
+                <img src={snailGif} alt="Snail GIF" style={{ width: '100px', height: '100px' }} />            </section>
 
             <section className='game-previews'>
-                <h2>Week 11 Game Previews</h2>
-                <p>Alright, now for the fun part! Here's a preview of the epic battles lined up for Week 11!</p>
-
+                <h2><strong>QUARTERFINALS</strong> - Where Dreams Come True (or get CRUSHED for half of us)
+                </h2>
                 <div className="game-preview">
-                    <p>🏈 <strong>Dean's Definitive Team (5-5-0 | 5th) vs El Duderino (4-6-0 | 9th)</strong></p>
-                    <p><em>Projected Scores:</em> 98.03 to 100.49</p>
-                    <p>In our <strong>RIVALRY OF THE WEEK</strong>, it’s the classic David vs. Goliath, if David was mildly disinterested and Goliath had a middle of the pack resume. Watch as these two fantasy juggernaut--ish--opponents...collide.</p>
+                    <p>🏈 <strong>Lucy's Head Coach (5-9-0 | 8th) vs No Barkley All Bite (9-5-0 | 1st)</strong></p>
+                    <p><em>Projected Scores:</em> 108.34 to 118.16</p>
+                    <p>Kyle's team: valiant underdogs or just waiting to be Bill's next discarded chew toy? Place your bets!</p>
+                </div>
+                <div className="game-preview">
+                    <p>🏈 <strong>Dean's Definitive Team (6-8-0 | 7th) vs Espresso LoPres(t)o (9-5-0 | 2nd)</strong></p>
+                    <p><em>Projected Scores:</em> 108.02 to 115.61</p>
+                    <p>It's the classic tale of hard work vs. sheer existence. Who will emerge victorious?
+                    </p>
                 </div>
 
                 <div className="game-preview">
-                    <p>🏈 <strong>Espresso LoPres(t)o (7-3-0 | 1st) The Maine Event (5-5-0 | 6th)</strong></p>
-                    <p><em>Projected Scores:</em> 98.42 to 118.69</p>
-                    <p>Craig's team is living up to their namesake, finishing out this show with a big finale.</p>
-                    <p>Meanwhile, first place is starting to look like the Defense Against the Dark Arts position. But the LoPresto's are looking to extend their tenure with another victory.</p>
-                    <p>Who's will end up pulling some a magical victory out this weekend?</p>
+                    <p>🏈 <strong>Lucy's Mom has got it going on (7-7-0 | 5th) vs The Passion of the Christ(ian) (8-6-0 | 4th)</strong></p>
+                    <p><em>Projected Scores:</em> 120.39 to 120.98</p>
+                    <p>Joanna and Eric, both sitting comfortably in the middle of the pack, are ready to bid a <i>top o the mornin</i> on their way up the winners bracket. Will Joanna's team dance a jig to victory, or will Eric’s squad find the pot of gold at the end of the rainbow?
+                    </p>
+                    <p>
+                        We'll see who has the luck of the Irish
+                    </p>
+
                 </div>
 
                 <div className="game-preview">
-                    <p>🏈 <strong>Lucy's Mom has got it going on (4-6-0 | 7th) vs No Barkley All Bite (6-4-0 | 4th)</strong></p>
-                    <p><em>Projected Scores:</em> 100.68 to 109.74</p>
-                    <p>One has all bite, one has a little kick. It's the battle of sass!</p>
-                    <p> But who will kick some...football!</p>
-                </div>
+                    <p>🏈 <strong>The Maine Event (6-6-0 | 6th) vs Hanah's Nifty Team (9-5-0 | 3rd)</strong></p>
+                    <p><em>Projected Scores:</em> 114.82 to 102.19</p>
+                    <p>This is a wildcard battle! Hanah, who reigned supreme atop the standings, faces a sudden twist of fate losing her quarterback and top receiver. Meanwhile, Craig, the patient strategist, has been quietly waiting for L.A. to morph into a healthy fantasy football dream team.
+                    </p>
+                    <p>Can Hanah's generic team put together a group of offbrand players to strike another victory? Or is Craig a <strong>DARK <em>RAM</em></strong> to win playoffs? </p>
 
+                </div>
                 <div className="game-preview">
-                    <p>🏈 <strong>Lucy's Head Coach (4-6-0 | 8th) vs The Unit (2-8-0 | 10th)</strong></p>
-                    <p><em>Projected Scores:</em> 104.48 to 97.11</p>
-                    <p>In a world where ladders are no longer for climbing and are now being used as less than ideal floatation devices, who will stop sinking into the depths of 'there's always next season'?</p>
+                    <p>🏈 <strong>RIP</strong></p>
+                    <p>This first week of playoffs, we say goodbye to two dear and recently departed league managers. The Unit and El Duderino may not have always been around, but when they were they took some truly inexplicable wins and especially helped continue to kick my useless team's battered carcass further into the dirt. </p>
+                    <p>See you in the next <i>live draft.</i></p>
                 </div>
-
-                <div className="game-preview">
-                    <p>🏈 <strong>The Passion of the Christ(ian) (6-4-0 | 3rd) vs Hanah's Nifty Team (7-3-0 | 2nd)</strong></p>
-                    <p><em>Projected Scores:</em> 121.53 to 116.42</p>
-                    <p>Eric and Hanah both made it to the infamous death zone of the Mount Everest that is our league, ascending beyond their opponents but finding out the perilous path at the top. Find out who has grit, endurance, and strategy--and who will be frozen stuck in the standings for others to pass! </p>
-                </div>
-            </section>
-            <p>
-                Best of luck to all in week 11!
-            </p>
-
-
-
-            <section>
-                <h2>Injured Reserve Note</h2>
-                <p>If you have players with red letters next to them, they may be IR eligible. If they are just questionable and might play, or after a certain amount of weeks, they are not. But while they are, you can essentially put two people on a special IR bench for players who cannot play due to injury for a few weeks. This way, you have extra spots on your team while you pick up other players for current matches.</p>
-
-                <p>However, this feature is reserved exclusively for injured players. If you're uncertain, try placing up to two players with red letters next to their names on IR. If the option isn't available, they are not eligible, indicating that they are likely a game-time decision and might still play. Feel free to message me if you're unsure about a specific player's eligibility or need assistance with this feature.</p>
             </section>
 
             <footer>
-                <p>Gear up and may the best teams win! Let's get this season rolling!</p>
-                <p>Best,<br />Kyle, Your Dedicated Commissioner</p>
+                <p>CHRISTMAS!!!</p>
+                <p>Best,<br />Kyle, Your <s>Dedicated</s> Dessicated Commissioner</p>
             </footer>
         </div>
     );
