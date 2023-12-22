d721ede0e5bcf1df515b996dd23a2a3321636de5
kjeffreys <kjeffreyscs@gmail.com>
2023-12-21 19:53:45 -0500

    pre-final

--- src/WelcomeMessage.js
+++ src/WelcomeMessage.js
@@ -25,35 +25,33 @@ function WelcomeMessage()
                 <h2><strong>SEMIFINALS</strong> - {'"There are too few of us now" --Treebeard'}
                 </h2>
                 <div className="game-preview">
-                    <p>🏈 <strong>The Passion of the Christ(ian) (8-6-0 | 4th) vs No Barkley All Bite (9-5-0 | 1st)</strong></p>
+                    <p>🏈 Semifinals: <strong>The Passion of the Christ(ian) (8-6-0 | 4th) vs No Barkley All Bite (9-5-0 | 1st)</strong></p>
                     <p><em>Projected Scores:</em> 120 to 121</p>
-                    <p>Friends who play together...end up slaughtering each other for the trophy.</p>
+                    <p>Friends turned frenemies for the semifinals. What will the victor taste? Frictory!</p>
                 </div>
                 <div className="game-preview">
-                    <p>🏈 <strong>Hanah's Nifty Team (9-5-0 | 3rd) vs Espresso LoPres(t)o (9-5-0 | 2nd)</strong></p>
+                    <p>🏈 Semifinals: <strong>Hanah's Nifty Team (9-5-0 | 3rd) vs Espresso LoPres(t)o (9-5-0 | 2nd)</strong></p>
                     <p><em>Projected Scores:</em> 117 to 110</p>
-                    <p>Ladies...
+                    <p>Witness a nautical tussle! Hanah, the reigning Port (Jervis) Authority, sailing her team against Alicen, Buffalo (Bills) Snow Lord. Will Hanah be collecting victory? Or will Alicen's blizzard of points leave her snowbound for the championship?
                     </p>
                 </div>
 
                 <div className="game-preview">
-                    <p>🏈 <strong>Lucy's Mom has got it going on (7-7-0 | 5th) vs Lucy's Head Coach (8-6-0 | 8th)</strong></p>
+                    <p>🏈 Consolation: <strong>Lucy's Mom has got it going on (7-7-0 | 5th) vs Lucy's Head Coach (8-6-0 | 8th)</strong></p>
                     <p><em>Projected Scores:</em> 120.39 to 120.98</p>
-                    <p>How poetic it is to be left for dead together as spouses.
+                    <p>A family feud that never made it to a reality TV show - with less drama and no bragging rights. Merry Christmas honey, glad to always end up by your side.
                     </p>
                 </div>
 
                 <div className="game-preview">
-                    <p>🏈 <strong>The Maine Event (6-6-0 | 6th) vs Dean's Definitive Team(6-8-0 | 7th)</strong></p>
+                    <p>🏈 Consolation: <strong>The Maine Event (6-6-0 | 6th) vs Dean's Definitive Team(6-8-0 | 7th)</strong></p>
                     <p><em>Projected Scores:</em> 114.82 to 102.19</p>
-                    <p>Oof
+                    <p>Like Jo and Kyle, somehow the strategy of lose now, win later did not pan out in the playoffs for these two. Best of luck in the consolation hunt. The prize? I don't know, a duck maybe? Oh, a Christmas goose! Where my Adventures of Sherlock Holmes fans at?
                     </p>
 
                 </div>
-                <div className="game-preview">
-                    <p>🏈 <strong>RIP</strong></p>
-                    <p>This first week of playoffs, we say goodbye to two dear and recently departed league managers. The Unit and El Duderino may not have always been around, but when they were they took some truly inexplicable wins and especially helped continue to kick my useless team's battered carcass further into the dirt. </p>
-                    <p>See you in the next <i>live draft.</i></p>
+                <div className="announcement">
+                    Best of luck to our semi-finalists!
                 </div>
             </section>
 
